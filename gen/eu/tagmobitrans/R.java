f73b719c167ec4084afd88cd875df4ac486d93eb
kikoo <kikoo@kikoo.(none)>
2011-05-20 16:35:25 +0200

    Displaying first part

--- gen/eu/tagmobitrans/R.java
+++ gen/eu/tagmobitrans/R.java
@@ -14,12 +14,16 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int arret=0x7f050000;
-        public static final int information=0x7f050002;
-        public static final int rechercher=0x7f050001;
+        public static final int afficher_donnee=0x7f050005;
+        public static final int arret=0x7f050002;
+        public static final int heure=0x7f050001;
+        public static final int information=0x7f050004;
+        public static final int nom=0x7f050000;
+        public static final int rechercher=0x7f050003;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int arret=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- src/eu/tagmobitrans/tag.java
+++ src/eu/tagmobitrans/tag.java
@@ -1,19 +1,7 @@
 package eu.tagmobitrans;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
+import java.util.HashMap;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -21,20 +9,26 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class tag extends Activity {
 	final static String URL = "http://tag.mobitrans.fr/";
+	final static int nbLinkInFooter = 5;//nb de liens en bas de page, histoire de parser tout les liens sans taper dedans
+	
 	EditText arret;
 	Button recherche;
 	TextView text;
+	ListView afficherDonnee;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         arret= (EditText)findViewById(R.id.arret);
         text= (TextView)findViewById(R.id.information);
+        afficherDonnee=(ListView)findViewById(R.id.afficher_donnee);
+        
         recherche= (Button)findViewById(R.id.rechercher);
         recherche.setOnClickListener(new OnClickListener() {
 			@Override
@@ -44,8 +38,10 @@ public class tag extends Activity {
 				TagVar tv = TagVar.getInstance(tag.this);
 				if(!tv.isInitialyzed())
 					tv.parseIndex(URL);
-				tv.parsePostPostArret(URL, arret.getText().toString());
+			/*Displaying data*/
+				SimpleAdapter mSchedule = new SimpleAdapter (tag.this.getBaseContext(), tv.parsePostPostArret(URL, arret.getText().toString()), R.layout.arret, new String[] {"nom", "heure"}, new int[] {R.id.nom, R.id.heure});
+		        afficherDonnee.setAdapter(mSchedule);
 			}
-		});
+		});        
     }
 }
